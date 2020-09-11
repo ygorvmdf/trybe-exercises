d2b40c4abd2435b6c8448628089b5bf86e92bd00
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-09-10 23:58:02 -0300

    adicionando valores para localstorage

--- bloco_5/dia_4/script.js
+++ bloco_5/dia_4/script.js
@@ -1,9 +1,8 @@
+// variables
 let body = document.body;
-let fontFamily = document.getElementById('font-family').value;
-let backColor = document.getElementById('background-color').value;
-let textColor = document.getElementById('text-color').value;
-let lineHeight = document.getElementById('line-height').value;
 let setButton = document.getElementById('set-style');
+
+// functions
 function setFontSize() {
     let fontSize = document.getElementById("font-size").value;
     body.style.fontSize = fontSize + "px";
@@ -24,8 +23,22 @@ function setTextColor() {
     body.style.color = textColor;
     localStorage.textColor = body.style.color;
 }
+function setLineHeight() {
+    let lineHeight = document.getElementById("line-height").value;
+    body.style.lineHeight = lineHeight;
+    localStorage.lineHeight = body.style.lineHeight;
+}
 
+// eventListeners
 setButton.addEventListener('click', setFontSize);
 setButton.addEventListener('click', setFontFamily);
 setButton.addEventListener('click', setBackgroundColor);
 setButton.addEventListener('click', setTextColor);
+setButton.addEventListener('click', setLineHeight);
+
+// loading with the localStorage values
+body.style.fontFamily = localStorage.fontFamily;
+body.style.fontSize = localStorage.fontSize;
+body.style.color = localStorage.textColor;
+body.style.backgroundColor = localStorage.backgroundColor;
+body.style.lineHeight = localStorage.lineHeight;
