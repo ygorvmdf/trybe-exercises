e5b6689da7bcca18579cd34bbbb7cb4ea6bdc878
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-09-28 18:05:29 -0300

    exercicio 1 bonus

---
+++ bloco_7/dia_2/bonus/exercise1.js
@@ -0,0 +1,37 @@
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+const allLessons = {
+    lesson1: Object.assign({}, lesson1),
+    lesson2: Object.assign({}, lesson2),
+    lesson3: Object.assign({}, lesson3)
+};
+
+const mathStudents = (object) => {
+  let totalStudents = 0;
+  for (key in object) {
+    if(object[key].materia === 'Matemática') {
+        totalStudents += object[key].numeroEstudantes;
+    }
+  }
+  return totalStudents;
+}
+
+console.log(mathStudents(allLessons));
