a4a6c6796ad7f7c6e5b9deba7974e54be90126d7
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-10-07 19:18:10 -0300

    exercicio 1 | transformando matriz de 2 dimensoes em 1 dimensao

--- bloco_8/dia_3/exercise1.js
+++ bloco_8/dia_3/exercise1.js
@@ -9,16 +9,9 @@ const arrays = [
 
 function flatten() {
   const flattenedArray = arrays.reduce((accumulator, current) => {
-    console.log(accumulator);
-    console.log(current);
     return  accumulator.concat(current)},
     []);
+    return flattenedArray;
 }
 
-console.log(flatten());
-
-// accumulator = [] | current = ["1", "2", "3"] >> concat === ["1", "2", "3"]
-// accumulator = ["1", "2", "3"] | current = [true] >> concat === ["1", "2", "3", true]
-// accumulator = ["1", "2", "3", true] | current = [4, 5, 6] >> concat
-
-// assert.deepEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
+assert.deepEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
