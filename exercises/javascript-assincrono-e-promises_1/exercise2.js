ca3b8b1b2cedf60f3e0f4e12e06c3501d71eaa84
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-10-21 18:20:11 -0300

    exercicio 2 | segunda ordem de execucao

---
+++ exercises/javascript-assincrono-e-promises_1/exercise2.js
@@ -0,0 +1,34 @@
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+const mars = {
+  name: "Mars",
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const venus = {
+  name: "Venus",
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const jupiter = {
+  name: "Jupiter",
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: "kilometers",
+  },
+};
+
+console.log(planetDistanceFromSun(mars)); // A
+setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // C
+setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // B
+
+// Mars is 227900000 kilometers apart from the Sun
+// Jupiter is 778500000 kilometers apart from the Sun
+// Venus is 108200000 kilometers apart from the Sun
