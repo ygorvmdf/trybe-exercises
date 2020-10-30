a58edb1a7f2e1008c51f5ef5cb46daed38417976
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-10-30 19:15:53 -0300

    exercicio 3 | testes usando funcoes mock com jest

--- exercises/testes-automatizados-com-jest_3/exercise3.test.js
+++ exercises/testes-automatizados-com-jest_3/exercise3.test.js
@@ -1,3 +1,26 @@
-const { boolean } = require("yargs");
+const { it, expect } = require("@jest/globals");
 
-console.log(Boolean([]));
+let randomNumber = () => Math.floor(Math.random * 101);
+
+randomNumber = jest.fn().mockImplementation((a, b, c) => {
+    if (typeof a !== "number" || typeof b !== "number" || typeof c !== "number") return 'invalid arguments';
+    return a * b * c;
+});
+
+it('should return the multiplication of three parameters', () => {
+    expect(randomNumber(1, 2, 3)).toBe(6);
+    expect(randomNumber(1, '2', 3)).toBe('invalid arguments');
+    expect(randomNumber(1, '2')).toBe('invalid arguments');
+    expect(randomNumber).toHaveBeenCalled();
+})
+
+it('should return the double value', () => {
+    randomNumber.mockReset();
+    randomNumber.mockImplementation((number) => typeof number === 'number' ? number * 2: 'invalid arguments');
+
+    expect(randomNumber(1)).toBe(2);
+    expect(randomNumber(2)).toBe(4);
+    expect(randomNumber).toHaveBeenCalled();
+    expect(randomNumber('2')).toBe('invalid arguments');
+    expect(randomNumber()).toBe('invalid arguments');
+})
