51e961e26cb0f60554b38705c343935645839b20
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-10-30 12:19:43 -0300

    exercicio 1 | testes assincronos com jest

--- exercises/testes-automatizados-com-jest_2/exercise1.test.js
+++ exercises/testes-automatizados-com-jest_2/exercise1.test.js
@@ -2,4 +2,10 @@ const uppercase = (str, callback) => {
   callback(str.toUpperCase());
 }
 
-
+test('if callback convert the string to lowercase', () => {
+    let expected = '';
+    uppercase('xablau', parameter => {
+      expected = parameter;
+    });
+    expect(expected).toBe('XABLAU');
+})
