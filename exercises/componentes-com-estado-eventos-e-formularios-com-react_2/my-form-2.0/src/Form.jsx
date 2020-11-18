2c651035f691fa6d1347af8795843ac0320939e5
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-11-17 21:26:43 -0300

    formularios com react

--- exercises/componentes-com-estado-eventos-e-formularios-com-react_2/my-form-2.0/src/Form.jsx
+++ exercises/componentes-com-estado-eventos-e-formularios-com-react_2/my-form-2.0/src/Form.jsx
@@ -24,33 +24,37 @@ class Form extends React.Component {
         }
     }
 
+    showAlert = ({ target }) => {
+        alert('Preencha com cuidado esta informação');
+    }
+
     render() {
         return (
             <form>
                 <fieldset>
                     <section id="name-input">
                         <label for="name">Nome</label>
-                        <input onChange={this.inputToUpperCase} name="nome" id="name" type="text" maxlength="40" required />
+                        <input onChange={this.inputToUpperCase} name="nome" id="name" type="text" maxlength="40" />
                     </section>
                     <section id="email-input">
                         <label for="email">Email</label>
-                        <input name="email" id="email" type="text" maxlength="50" required />
+                        <input name="email" id="email" type="text" maxlength="50" />
                     </section>
                     <section id="cpf-input">
                         <label for="cpf">CPF</label>
-                        <input name="cpf" id="cpf" type="text" maxlength="11" required />
+                        <input name="cpf" id="cpf" type="text" maxlength="11" />
                     </section>
                     <section id="adress-input">
                         <label for="adress">Endereço</label>
-                        <input onChange={this.removeSpecialCharacters} name="endereco" id="adress" type="text" maxlength="200" required />
+                        <input onChange={this.removeSpecialCharacters} name="endereco" id="adress" type="text" maxlength="200" />
                     </section>
                     <section id="city-input">
                         <label for="vity">Cidade</label>
-                        <input onBlur={this.removeIfBeginsWithNumber} name="cidade" id="city" type="text" maxlength="28" required />
+                        <input onBlur={this.removeIfBeginsWithNumber} name="cidade" id="city" type="text" maxlength="28" />
                     </section>
                     <section id="city-input">
                         <label for="vity">Cidade</label>
-                        <select required>
+                        <select>
                             <option>Acre</option>
                             <option>Alagoas</option>
                             <option>Amapá</option>
@@ -81,12 +85,27 @@ class Form extends React.Component {
                         </select>
                         <section id="tipo-input">
                             <label for="casa">Casa</label>
-                            <input id="casa" type="radio" name="tipo" value="casa" checked/>
+                            <input id="casa" type="radio" name="tipo" value="casa" checked />
                             <label for="apartamento">Apartamento</label>
                             <input id="apartamento" type="radio" name="tipo" value="apartamento" />
                         </section>
                     </section>
                 </fieldset>
+                <fieldset>
+                    <section>
+                        <label for="curriculum">Resumo do currículo</label>
+                        <textarea id="curriculum" maxlength="1000"></textarea>
+                    </section>
+                    <section>
+                        <label for="cargo">Cargo</label>
+                        <textarea id="cargo" onMouseEnter={this.showAlert} maxlength="40"></textarea>
+                    </section>
+                    <section>
+                        <label for="description">Descrição do cargo</label>
+                        <input id="description" type="text" maxlength="500" />
+                    </section>
+                </fieldset>
+                <button></button>
             </form>
         )
     }
